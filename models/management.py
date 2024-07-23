cc9cd426aa16f921353c73d8defb468f39c40ecf
Nguyen Dinh Loi <loind@thuanhai.com.vn>
2024-07-23 15:02:11 +0700

    fix: pool cursor

--- models/management.py
+++ models/management.py
@@ -320,50 +320,48 @@ class BackupManagement(models.Model):
         # else:
         #     file_content = db_datas
         # extension = mimetypes.guess_extension(mimetype)
-        new_cr = self.pool.cursor()
-        self = self.with_env(self.env(cr=new_cr))
-        attachment = self.get_attachment_by_id(new_cr, id)
-        # attachment.ensure_one()
-        year = utils.get_year(str(attachment.create_date))
-        upload_path = (
-            f"{upload_url}/{host_name}/{attachment.res_model}/{year}/{attachment.name}"
-        )
-        _logger.info(f"upload_path: {upload_path}")
+        with self.pool.cursor() as new_cr:
+            # new_cr = self.pool.cursor()
+            self = self.with_env(self.env(cr=new_cr))
+            attachment = self.get_attachment_by_id(new_cr, id)
+            # attachment.ensure_one()
+            year = utils.get_year(str(attachment.create_date))
+            upload_path = f"{upload_url}/{host_name}/{attachment.res_model}/{year}/{attachment.name}"
+            _logger.info(f"upload_path: {upload_path}")
 
-        sharepoint_res = SharePoint().upload_file_to_sharepoint(
-            upload_path,
-            client_key,
-            client_secret,
-            tenant_id,
-            scope,
-            behavior,
-            attachment.db_datas,
-        )
-        download_url = None
-        if (
-            sharepoint_res.status_code == http.HTTPStatus.OK
-            or sharepoint_res.status_code == http.HTTPStatus.CREATED
-        ):
-            json_data = sharepoint_res.json()
-            download_url = json_data["@microsoft.graph.downloadUrl"]
-            sharepoint_id = json_data["id"]
-            _logger.info(f"sharepoint_id: {sharepoint_id}")
-            # self.update_atts(attachment_id, download_url, sharepoint_id)
-            # if store_fname:
-            #     utils.delete_file(file_path)
-        else:
-            self.env["log.backup"].create(
-                {
-                    "backup_id": backup_id,
-                    "status_code": sharepoint_res.status_code,
-                    "message": sharepoint_res.json()["error"]["message"],
-                    "log_type": const.SHAREPOINT_TYPE,
-                    "url": download_url,
-                    "attachment_name": attachment.name,
-                }
+            sharepoint_res = SharePoint().upload_file_to_sharepoint(
+                upload_path,
+                client_key,
+                client_secret,
+                tenant_id,
+                scope,
+                behavior,
+                attachment.db_datas,
             )
-        new_cr.commit()
-        new_cr.close()
+            download_url = None
+            if (
+                sharepoint_res.status_code == http.HTTPStatus.OK
+                or sharepoint_res.status_code == http.HTTPStatus.CREATED
+            ):
+                json_data = sharepoint_res.json()
+                download_url = json_data["@microsoft.graph.downloadUrl"]
+                sharepoint_id = json_data["id"]
+                _logger.info(f"sharepoint_id: {sharepoint_id}")
+                # self.update_atts(attachment_id, download_url, sharepoint_id)
+                # if store_fname:
+                #     utils.delete_file(file_path)
+            else:
+                self.env["log.backup"].create(
+                    {
+                        "backup_id": backup_id,
+                        "status_code": sharepoint_res.status_code,
+                        "message": sharepoint_res.json()["error"]["message"],
+                        "log_type": const.SHAREPOINT_TYPE,
+                        "url": download_url,
+                        "attachment_name": attachment.name,
+                    }
+                )
+            new_cr.commit()
         return True
 
     def open_log_wizard(self):
